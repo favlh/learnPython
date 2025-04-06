e459b4d2c33438ae6c4dcd4deaf1dccb0420768f
favlh <firjat809@gmail.com>
2025-04-06 08:48:12 +0700

    p

---
+++ Ngoding/Belajar Python/def.py
@@ -0,0 +1,6 @@
+# Using def to define a function in python
+
+def hello_world():
+    print("Hello Mom!!")
+    
+hello_world()
---
+++ Ngoding/Belajar Python/helloMom.py
@@ -0,0 +1,4 @@
+# First Coding In Python
+
+print("Hello Mum")
+print("This is my first coding in Python !!")
