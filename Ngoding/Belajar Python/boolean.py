d55a7122db6dc13e806f4276d0e580f18ffffb8a
favlh <firjat809@gmail.com>
2025-04-06 08:45:22 +0700

    p

--- Ngoding/Belajar Python/boolean.py
+++ Ngoding/Belajar Python/boolean.py
@@ -1,8 +1,7 @@
-# tipe data boolean
+# tipe data boolean di python
 
 hidup = True
 mati = False
 
 print(hidup)
 print(mati)
-
--- Ngoding/Belajar Python/input_data_number.py
+++ Ngoding/Belajar Python/input_data_number.py
@@ -6,7 +6,8 @@ angka_pertama = int(input("masukkan nomor pertama: "))
 angka_kedua = int(input("masukkan nomor kedua: "))
 angka_ketiga = int(input("masukkan nomor ketiga: "))
 
-hasil = angka_pertama + angka_kedua - angka_ketiga
+hasil_pertama = angka_pertama + angka_kedua - angka_ketiga
+hasil_kedua = angka_pertama * angka_kedua / angka_ketiga
 
-print(f"jadi hasilnya adalah: {hasil}")
-
+print(f"hasil pertamanya adalah: {hasil_pertama}")
+print(f"hasil keduanya adalah: {hasil_kedua}")
